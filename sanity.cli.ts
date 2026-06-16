b20c01be37ab84a9e5a152e726d62fdd8787aa73
shuja uddin <128310375+shuja609@users.noreply.github.com>
2026-06-17 02:29:23 +0500

    feat: add styled-components package configuration file with version 6.4.2

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -7,11 +7,7 @@ export default defineCliConfig({
   },
   studioHost: 'thcpensbulk',
   deployment: {
-    appId: 'g906g7udef0gjgmopchlmsqb',
-    /**
-     * Enable auto-updates for studios.
-     * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
-     */
+    appId: 'cltbqadknswifytfrxocjzhf',
     autoUpdates: true,
   }
 })
